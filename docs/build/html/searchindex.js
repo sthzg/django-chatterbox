3fc15afb83453e5ee754165a6b3c613c57189ec9
sthzg <sthzg@gmx.net>
2015-04-12 10:54:50 +0200

    Docs -- added sphinx-quickstart docs

---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:46,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to django-chatterbox&#8217;s documentation!"],titleterms:{chatterbox:0,django:0,document:0,indic:0,tabl:0,welcom:0}})
